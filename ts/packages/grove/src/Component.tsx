219f3aedd8471c726948e0fbfd495b511f893ef1
kmirzavaziri <kmirzavaziri@gmail.com>
2025-04-03 17:23:29 +0400

    .

--- ts/packages/grove/src/Component.tsx
+++ ts/packages/grove/src/Component.tsx
@@ -8,11 +8,12 @@ export const componentRegistry = new Map<string, React.ComponentType<Struct>>();
 export interface ComponentProps {
     type?: string;
     key?: string;
-    path?: string[];
     role?: string;
     props?: Struct;
     input?: Input;
     children?: ComponentProps[];
+
+    path?: string[];
 }
 
 export const Component: React.FC<{props: ComponentProps}> = ({props}) => {
@@ -38,18 +39,43 @@ export const Component: React.FC<{props: ComponentProps}> = ({props}) => {
     );
 };
 
+export const modifyComponentProps = (dest: ComponentProps, src: ComponentProps, patch: boolean): void => {
+    if (patch) {
+        patchComponentProps(dest, src);
+    } else {
+        overrideComponentProps(dest, src);
+    }
+}
+
 export const patchComponentProps = (dest: ComponentProps, src: ComponentProps): void => {
     if (src.type) {
         dest.type = src.type;
     }
+
     if (src.key) {
         dest.key = src.key;
     }
+
     if (src.role) {
         dest.role = src.role;
     }
+
     dest.props = {...dest.props, ...src.props};
+
+    if (src.input) {
+        dest.input = {...dest.input, ...src.input};
+    }
+
     if (src.children?.length) {
         dest.children = src.children;
     }
+};
+
+export const overrideComponentProps = (dest: ComponentProps, src: ComponentProps): void => {
+    dest.type = src.type;
+    dest.key = src.key;
+    dest.role = src.role;
+    dest.props = src.props;
+    dest.input = src.input;
+    dest.children = src.children;
 };
